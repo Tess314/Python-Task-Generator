4fde002eee016a75ef1e9c8eebcbee7029383bc8
Tess Watt <71218139+Tess314@users.noreply.github.com>
2020-10-02 12:41:36 +0100

    Update Lockdown Task Generator.py
    Updated motto for Scotland

--- Lockdown Task Generator.py
+++ Lockdown Task Generator.py
@@ -29,24 +29,26 @@ master = Tk()
 master.title("Coronavirus App")
 
 Label(master, text="CURE CORONAVIRUS BOREDOM").grid(row=0, column=0, padx=10)
-Label(master, text="STAY HOME > PROTECT THE NHS > SAVE LIVES", fg="blue", bg="white").grid(row=1, column=0, padx=10)
+#updated motto for Scotland
+Label(master, text="REMEMBER F.A.C.T.S.", fg="blue", bg="white").grid(row=1, column=0, padx=10)
+Label(master, text="Face coverings > Avoid crowding > Clean hands > Two metres > Self isolate", fg="blue", bg="white").grid(row=2, column=0, padx=10)
 
-Label(master, text="Do I have symptoms?").grid(row=2, pady=10)
+Label(master, text="Do I have symptoms?").grid(row=3, pady=10)
 option = StringVar(master)
 option.set("View")
 #updated symptoms list 02/10/2020
 symptom = OptionMenu(master,option, "Breathless", "Continuous Cough", "Fever", "Loss/change of taste/smell", "Shivering") #alphabetised
-symptom.grid(row=2, column=1, sticky=W)
+symptom.grid(row=3, column=1, sticky=W)
 
-Button(master, text='Check Symptom', command=checkSymptom).grid(row=2, column=2, sticky=W, pady=10)
+Button(master, text='Check Symptom', command=checkSymptom).grid(row=3, column=2, sticky=W, pady=10)
 
-Label(master, text="Choose a task").grid(row=3)
-Button(master, text='Click here', command=chooseTask).grid(row=3, column=1, sticky=W, pady=10)
+Label(master, text="Choose a task").grid(row=4)
+Button(master, text='Click here', command=chooseTask).grid(row=4, column=1, sticky=W, pady=10)
 
-Label(master, text="Get Support").grid(row=4)
-Button(master, text='Get quote', command=giveSupport).grid(row=4, column=1, sticky=W, pady=10)
+Label(master, text="Get Support").grid(row=5)
+Button(master, text='Get quote', command=giveSupport).grid(row=5, column=1, sticky=W, pady=10)
 
-Label(master, text="Watch a Nice Video").grid(row=5)
-Button(master, text='View video', command=watchVid).grid(row=5, column=1, sticky=W, pady=10)
+Label(master, text="Watch a Nice Video").grid(row=6)
+Button(master, text='View video', command=watchVid).grid(row=6, column=1, sticky=W, pady=10)
 
 mainloop()
